534f08d29d1e491eb006f922b2b6f63522d460a5
Jake Beckerle <tekwiz123@gmail.com>
2025-08-26 12:26:19 -0700

    Update _advanced.recipes.tsx

--- src/routes/_advanced.recipes.tsx
+++ src/routes/_advanced.recipes.tsx
@@ -1,5 +1,5 @@
 import { createFileRoute } from '@tanstack/react-router';
-import Recipes from '../pages/AdvancedMode/RecipesPage/recipes';
+import Recipes from '../pages/AdvancedMode/RecipesPage/recipes.jsx';
 
 export const Route = createFileRoute('/_advanced/recipes')({
   component: Recipes,
